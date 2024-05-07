8855c2c16fa1fd9fed73c12b4b9e872a4a87a89a
Andreas Lang <andreas.lang@grommunio.com>
2024-05-07 22:57:18 +0200

    Add return statement to getFreeBusyForEmail
    References: Issue #46

--- lib/GrommunioSchedulePlugin.php
+++ lib/GrommunioSchedulePlugin.php
@@ -2,14 +2,21 @@
 /*
  * SPDX-License-Identifier: AGPL-3.0-only
  * SPDX-FileCopyrightText: Copyright 2016 - 2018 Kopano b.v.
- * SPDX-FileCopyrightText: Copyright 2020 grommunio GmbH
+ * SPDX-FileCopyrightText: Copyright 2020-2024 grommunio GmbH
  *
  * Checks Free/Busy information of requested recipients.
  */
 
 namespace grommunio\DAV;
 
-class GrommunioSchedulePlugin extends \Sabre\CalDAV\Schedule\Plugin {
+use Sabre\CalDAV\Schedule\Plugin;
+use Sabre\VObject\Component;
+use Sabre\VObject\Reader;
+
+class GrommunioSchedulePlugin extends Plugin {
+	private $logger;
+	protected $gDavBackend;
+
 	/**
 	 * Constructor.
 	 */
@@ -28,29 +35,29 @@ class GrommunioSchedulePlugin extends \Sabre\CalDAV\Schedule\Plugin {
 	 *
 	 * @return array
 	 */
-	protected function getFreeBusyForEmail($email, \DateTimeInterface $start, \DateTimeInterface $end, \Sabre\VObject\Component $request) {
+	protected function getFreeBusyForEmail($email, \DateTimeInterface $start, \DateTimeInterface $end, Component $request) {
 		$this->logger->trace("email: %s - start: %d - end: %d", $email, $start->getTimestamp(), $end->getTimestamp());
 
 		$addrbook = $this->gDavBackend->GetAddressBook();
 		$email = preg_replace('!^mailto:!i', '', $email);
 		$search = [[PR_DISPLAY_NAME => $email]];
 		$userarr = mapi_ab_resolvename($addrbook, $search, EMS_AB_ADDRESS_LOOKUP);
-		if (!$userarr) {
-			return [
-				'request-status' => '3.7;Could not find principal',
-				'href' => 'mailto:' . $email,
-			];
+		if (!empty($userarr)) {
+			$result = mapi_getuserfreebusyical($this->gDavBackend->GetSession(), $userarr[0][PR_ENTRYID], $start->getTimestamp(), $end->getTimestamp());
+			if ($result) {
+				$vcalendar = Reader::read($result, Reader::OPTION_FORGIVING);
+
+				return [
+					'calendar-data' => $vcalendar,
+					'request-status' => '2.0;Success',
+					'href' => 'mailto:' . $email,
+				];
+			}
 		}
 
-		$result = mapi_getuserfreebusyical($this->gDavBackend->GetSession(), $userarr[0][PR_ENTRYID], $start->getTimestamp(), $end->getTimestamp());
-		if ($result) {
-			$vcalendar = \Sabre\VObject\Reader::read($result, \Sabre\VObject\Reader::OPTION_FORGIVING);
-
-			return [
-				'calendar-data' => $vcalendar,
-				'request-status' => '2.0;Success',
-				'href' => 'mailto:' . $email,
-			];
-		}
+		return [
+			'request-status' => '3.7;Could not find principal',
+			'href' => 'mailto:' . $email,
+		];
 	}
 }
